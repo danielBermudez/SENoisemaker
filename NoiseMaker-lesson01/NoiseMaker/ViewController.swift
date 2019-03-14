2d1d881c973ebb3939ed990e7cb42372d074dc63
Daniel Bermúdez <daniel.bermudez@endava.com>
2019-03-14 14:02:21 -0500

    Lesson04 Complete

--- NoiseMaker-lesson01/NoiseMaker/ViewController.swift
+++ NoiseMaker-lesson01/NoiseMaker/ViewController.swift
@@ -15,6 +15,9 @@ class ViewController: UIViewController {
     @IBAction func playGuitar(_ sender: UIButton) {
         if let url = Bundle.main.url(forResource: "guitar", withExtension: "wav"){
             guitarPlayer = try? AVAudioPlayer(contentsOf: url)
+            guitarPlayer?.volume = 0.5
+            guitarPlayer?.pan = -1.0
+            guitarPlayer?.numberOfLoops = 2
             guitarPlayer?.play()
             
         }
@@ -24,18 +27,29 @@ class ViewController: UIViewController {
     @IBAction func playApplause(_ sender: UIButton) {
         if let url = Bundle.main.url(forResource: "applause", withExtension: "wav"){
             applausePlayer = try? AVAudioPlayer(contentsOf: url)
+           applausePlayer?.volume = 0.7
+            applausePlayer?.pan = 0
+            applausePlayer?.numberOfLoops = 2
             applausePlayer?.play()
         }
     }
     @IBAction func playMonster(_ sender: UIButton) {
         if let url = Bundle.main.url(forResource: "monster", withExtension: "wav"){
             monsterPlayer = try? AVAudioPlayer(contentsOf: url)
+            monsterPlayer?.volume = 1
+            monsterPlayer?.pan = 1.0
+           monsterPlayer?.numberOfLoops = 5
             monsterPlayer?.play()
         }
     }
     @IBAction func playBubbles(_ sender: UIButton) {
         if let url = Bundle.main.url(forResource: "bubbles", withExtension: "wav"){
             bubblesPlayer = try? AVAudioPlayer(contentsOf: url)
+            bubblesPlayer?.volume = 1
+           bubblesPlayer?.pan = 0
+            bubblesPlayer?.numberOfLoops = 1
+           
+        
             bubblesPlayer?.play()
         }
     }
