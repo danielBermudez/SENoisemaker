01046f593beec533cf39b920c60b56c57b16c9b8
Daniel Bermúdez <daniel.bermudez@endava.com>
2019-03-14 16:38:39 -0500

    Lesson06Complete

--- NoiseMaker-lesson01/NoiseMaker/NoiseMaker.swift
+++ NoiseMaker-lesson01/NoiseMaker/NoiseMaker.swift
@@ -13,31 +13,36 @@ class Noisemaker{
     private var applausePlayer : AVAudioPlayer?
     private var monsterPlayer : AVAudioPlayer?
     private var bubblesPlayer : AVAudioPlayer?
-    func playGuitar() {
+    init(){
         if let url = Bundle.main.url(forResource: "guitar", withExtension: "wav"){
             guitarPlayer = try? AVAudioPlayer(contentsOf: url)
-            guitarPlayer?.play()
-            
         }
         
+        if let url2 = Bundle.main.url(forResource: "applause", withExtension: "wav"){
+            applausePlayer = try? AVAudioPlayer(contentsOf: url2)
+        }
+        
+        if let url3 = Bundle.main.url(forResource: "monster", withExtension: "wav"){
+            monsterPlayer = try? AVAudioPlayer(contentsOf: url3)
+        }
+        
+        if let url4 = Bundle.main.url(forResource: "bubbles", withExtension: "wav"){
+            bubblesPlayer = try? AVAudioPlayer(contentsOf: url4)
+        }
+        
+            }
+    
+    func playGuitar() {
+            guitarPlayer?.play()
     }
     
    func playApplause() {
-        if let url = Bundle.main.url(forResource: "applause", withExtension: "wav"){
-            applausePlayer = try? AVAudioPlayer(contentsOf: url)
             applausePlayer?.play()
-        }
     }
      func playMonster() {
-        if let url = Bundle.main.url(forResource: "monster", withExtension: "wav"){
-            monsterPlayer = try? AVAudioPlayer(contentsOf: url)
             monsterPlayer?.play()
-        }
     }
     func playBubbles() {
-        if let url = Bundle.main.url(forResource: "bubbles", withExtension: "wav"){
-            bubblesPlayer = try? AVAudioPlayer(contentsOf: url)
             bubblesPlayer?.play()
-        }
     }
 }
