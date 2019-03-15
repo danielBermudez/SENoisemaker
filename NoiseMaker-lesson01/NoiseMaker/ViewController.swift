12b7bdad20e0dbd859afc59fad82945ba6d32e8c
Daniel Bermúdez <daniel.bermudez@endava.com>
2019-03-15 09:43:51 -0500

    Lesson09 Compllete

--- NoiseMaker-lesson01/NoiseMaker/ViewController.swift
+++ NoiseMaker-lesson01/NoiseMaker/ViewController.swift
@@ -9,20 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
     let noiseMaker = Noisemaker()
-    @IBAction func playGuitar(_ sender: UIButton) {
-        noiseMaker.play(index: 0)
-        
+    
+    @IBAction func playSound(_ sender: UIButton) {
+        // play the right sound
+        noiseMaker.play(index: sender.tag)
     }
     
-    @IBAction func playApplause(_ sender: UIButton) {
-        noiseMaker.play(index: 1)
-    }
-    @IBAction func playMonster(_ sender: UIButton) {
-        noiseMaker.play(index: 2)
-    }
-    @IBAction func playBubbles(_ sender: UIButton) {
-         noiseMaker.play(index: 3)
-    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
